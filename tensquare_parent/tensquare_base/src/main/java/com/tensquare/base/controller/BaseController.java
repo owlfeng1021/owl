77f1f944aea54b10fe23a232c8ada8e5620d4db7
owlfeng1021 <1033571362@qq.com>
2019-03-10 20:08:22 +0800

    2019/3/10号写的内容

--- tensquare_parent/tensquare_base/src/main/java/com/tensquare/base/controller/BaseController.java
+++ tensquare_parent/tensquare_base/src/main/java/com/tensquare/base/controller/BaseController.java
@@ -2,10 +2,14 @@ package com.tensquare.base.controller;
 
 import com.tensquare.base.pojo.Label;
 import com.tensquare.base.service.LabelService;
+import entity.PageResult;
 import entity.Result;
 import entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -45,8 +49,11 @@ public class BaseController {
 
         return new Result(true,StatusCode.OK,"查询成功",labelService.findSearch(label));
     }
-//    @PostMapping("/search/{page}/{size}")
-//    public
+      @PostMapping("/search/{page}/{size}")
+      public Result findPage(@RequestBody Label label ,@PathVariable int page ,@PathVariable int size){
+          Page<Label> labelPage= labelService.findQuery(label,page,size);
+          return new Result(true,StatusCode.OK,"查询成功", new PageResult<Label>(labelPage.getTotalElements(),labelPage.getContent()));
+      }
 
 
 
--- tensquare_parent/tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
+++ tensquare_parent/tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
@@ -3,6 +3,9 @@ package com.tensquare.base.service;
 import com.tensquare.base.dao.LabelDao;
 import com.tensquare.base.pojo.Label;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -56,11 +59,11 @@ public class LabelService {
             public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
                 List<Predicate> list=new ArrayList<>();
                 if (!StringUtils.isEmpty(label.getLabelname())){
-                    Predicate predicate =cb.like(root.get("labelname").as(String.class),"%"+label.getLabelname()+"%");
+                    Predicate predicate =cb.like(root.get("labelname").as(String.class),"%"+label.getLabelname()+"%"); // where labelname like %labelname%
                     list.add(predicate);
                 }
                 if(!StringUtils.isEmpty(label.getState())){
-                    Predicate predicate =cb.equal(root.get("state").as(String.class),label.getState());
+                    Predicate predicate =cb.equal(root.get("state").as(String.class),label.getState()); // and state = 1
                     list.add(predicate);
                 }
                 Predicate[] parr =new Predicate[list.size()];
@@ -70,5 +73,32 @@ public class LabelService {
         });
     }
 
-
+    public Page<Label> findQuery(Label label, int page, int size) {
+        //封装分页对象
+        Pageable pageable=PageRequest.of(page-1,size);
+        return  labelDao.findAll(new Specification<Label>() {
+                /**
+                 *
+                 * @param root 根对象 也就是把条件封装到那个对象中 where 类名=label。getid
+                 * @param query 封装的都是查询关键字 比如 group by' order by
+                 * @param cb 用来封装条件对象 如果直接返回null 表示 不需要任何的条件
+                 * @return
+                 */
+                @Override
+                public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                    List<Predicate> list=new ArrayList<>();
+                    if (!StringUtils.isEmpty(label.getLabelname())){
+                        Predicate predicate =cb.like(root.get("labelname").as(String.class),"%"+label.getLabelname()+"%"); // where labelname like %labelname%
+                        list.add(predicate);
+                    }
+                    if(!StringUtils.isEmpty(label.getState())){
+                        Predicate predicate =cb.equal(root.get("state").as(String.class),label.getState()); // and state = 1
+                        list.add(predicate);
+                    }
+                    Predicate[] parr =new Predicate[list.size()];
+                    parr=list.toArray(parr);
+                    return cb.and(parr);
+                }
+            },pageable);
+    }
 }
